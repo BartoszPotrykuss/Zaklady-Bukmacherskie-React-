0f66f97b07bfbcf61574fad799a03003dd70e40d
ciornyyy <b.potrykus@o2.pl>
2024-04-17 10:41:27 +0200

    footer change

--- src/komponenty/Footer.js
+++ src/komponenty/Footer.js
@@ -1,12 +1,12 @@
 import '../style/Footer.css'
-import Foot1 from './Foot1';
+import FootItem from './FootItem';
 
 function Footer() {
     return (
         <footer>
-            <Foot1 tytul = "O firmie" p1 = "Historia" p2 = "Zespół" p3 = "Dołącz do nas" />
-            <Foot1 tytul = "Kontakt" p1 = "Telefon" p2 = "Adres e-mail" p3 = "Formularz kontaktowy" />
-            <Foot1 tytul = "Informacje" p1 = "Polityka prywatności" p2 = "Regulamin" p3 = "FAQ" />
+            <FootItem tytul = "O firmie" p1 = "Historia" p2 = "Zespół" p3 = "Dołącz do nas" />
+            <FootItem tytul = "Kontakt" p1 = "Telefon" p2 = "Adres e-mail" p3 = "Formularz kontaktowy" />
+            <FootItem tytul = "Informacje" p1 = "Polityka prywatności" p2 = "Regulamin" p3 = "FAQ" />
         </footer>
     );
 }
